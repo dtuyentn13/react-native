db7267ce3d38048c5ec9ed1fccd96070655d4106
dtuyentn13 <55440978+dtuyentn13@users.noreply.github.com>
2019-11-08 16:19:10 +0700

    no message

--- TUTORIALPROJECT/index.js
+++ TUTORIALPROJECT/index.js
@@ -3,7 +3,19 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//import {name as appName} from './app.json';
+import React, { Component } from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import allReducers from './reducers';
+import TaskManagerComponent from './components/TaskManagerComponent'
+
+let store = createStore(allReducers);
+const App = () => (
+    <Provider store={store}>
+    <TaskManagerComponent />
+    </Provider>
+);
+AppRegistry.registerComponent('TUTORIALPROJECT', () => App);
